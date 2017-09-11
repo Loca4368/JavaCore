950cc934967ab508b0a839ecddaaaa32f18dd8d6
Ming <qiuming4368@gmail.com>
2017-09-11 18:47:56 +1000

    errorAndException

---
+++ errorAndException/MyCustomException.java
@@ -0,0 +1,13 @@
+package errorAndException;
+
+
+//Construct an custom exception
+public class MyCustomException extends Exception{
+	
+	public MyCustomException(String msg)
+	{
+		super(msg);
+	}
+
+
+}
